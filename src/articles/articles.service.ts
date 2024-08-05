a5b51fe424cb002da21c6d2b842566ed9edca15a
Qamar Zaman <qamarzk12345@gmail.com>
2024-08-05 16:12:34 +0500

    crud and login using node js prisma swagg is complete

---
+++ src/articles/articles.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { CreateArticleDto } from './dto/create-article.dto';
+import { UpdateArticleDto } from './dto/update-article.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class ArticlesService {
+  constructor(private prisma: PrismaService) {}
+
+  findDrafts() {
+    return this.prisma.article.findMany({ where: { published: false } });
+  }
+
+
+  create(createArticleDto: CreateArticleDto) {
+    return this.prisma.article.create({ data: createArticleDto });
+  }
+
+  findAll() {
+    return this.prisma.article.findMany({ where: { published: true } });
+
+  }
+
+  findOne(id: number) {
+    return this.prisma.article.findUnique({
+      where: { id },
+      include: {
+        author: true,
+      },
+    });
+  }
+
+  update(id: number, updateArticleDto: UpdateArticleDto) {
+    return this.prisma.article.update({
+      where: { id },
+      data: updateArticleDto,
+    });
+  }
+
+  remove(id: number) {
+    return this.prisma.article.delete({ where: { id } });
+
+  }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,33 @@
+//src/auth/auth.service.ts
+import { AuthEntity } from './entity/auth.entity';
+import { PrismaService } from './../prisma/prisma.service';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService, private jwtService: JwtService) {}
+
+  async login(email: string, password: string): Promise<AuthEntity> {
+    const user = await this.prisma.user.findUnique({ where: { email } });
+
+    if (!user) {
+      throw new NotFoundException(`No user found for email: ${email}`);
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid password');
+    }
+
+    return {
+      accessToken: this.jwtService.sign({ userId: user.id }),
+    };
+  }
+}
